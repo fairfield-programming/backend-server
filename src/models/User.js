3745ea97af4bfcc460b5c112458ca00abe2c33bf
William McGonagle <wpmcgonagle@gmail.com>
2021-12-24 10:59:24 -0500

    🧪 Added Onto Users

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,10 @@ module.exports = function (sequelize, DataTypes) {
 	return sequelize.define('User', {
 		username: DataTypes.STRING,
 		password: DataTypes.TEXT,
-		email: DataTypes.TEXT
+		email: DataTypes.TEXT,
+		biography: DataTypes.TEXT,
+		profilePicture: DataTypes.TEXT,
+		status: DataTypes.STRING,
 	})
 
 };
