a95946095b3c18d799c7501eb2c1d3854b189c3c
Neil <30356902+NeilChaudhari@users.noreply.github.com>
2021-12-24 18:18:48 -0500

    🔑Added Set Password

---
+++ src/routes/User/setPass.js
@@ -0,0 +1,31 @@
+module.exports = (req, res) => {
+    if (req.params.id == undefined) return res.status(400).send("Not All Parameters Provided.")
+
+    User.findOne({
+		where: {
+			id: req.params.id
+		}
+	}).then(function (userData) {
+	
+		bcrypt.compare(req.body.password, userData.password, function (err, result) {
+
+            if (err) {
+
+				console.log(err) 
+				return res.status(500).send("Internal Server Error.")
+
+			}
+            if (!result) return res.status(403).send("Incorrect Password.")
+            
+            await userData.update({password:req.body.newPassword})
+            await userData.save()
+
+        })
+	
+	}).catch(function (error) {
+	
+		console.log(error);
+		return res.status(500).send("Internal Server Error.")
+	
+	})
+}
