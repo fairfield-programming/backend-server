e2b588e4197113693d377d8a2a4fd34b27201c1b
Neil <30356902+NeilChaudhari@users.noreply.github.com>
2021-12-24 18:32:02 -0500

    🔨 Fixed Bug in Index

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ app.get("/user/", require('./routes/User/listUsers'));
 app.post("/user/signup", require('./routes/User/signup'));
 app.post("/user/login", require('./routes/User/login'));
 app.post("/user/:id/status", require('./routes/User/setStatus'));
-app.post('/user/:id/setPass','./routes/User/setPass');
+app.post('/user/:id/setPass',require('./routes/User/setPass'));
 
 // Start Server
 if (process.env.NODE_ENV != 'test') {
