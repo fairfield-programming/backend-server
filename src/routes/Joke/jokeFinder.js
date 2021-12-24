a95beca4f6453cf3b1440c3eaf71ad67f136673e
William McGonagle <wpmcgonagle@gmail.com>
2021-12-23 21:51:51 -0500

    🎉 Almost Done with Lib

--- src/routes/Joke/jokeFinder.js
+++ src/routes/Joke/jokeFinder.js
@@ -15,10 +15,37 @@ function getAllJokes() {
 // Gets a Joke at an Index
 function getJokeAtIndex(id) {
 
+    // Get all of the Jokes
+    var jokes = getAllJokes();
+    var jokeCount = jokes.length;
 
+    // Check if Id is out of Range
+    if (typeof id != 'number') return false;
+    if (id < 0) return false; 
+    if (id >= jokeCount) return false;
+
+    // Return the Joke
+    return jokes[id];
+
+}
+
+// Get the Joke Count
+function getJokeCount() {
+
+    // Return the Count
+    return getAllJokes().length;
+
+}
+
+// Get a Random Joke
+function getRandomJoke() {
+
+    
 
 }
 
 module.exports = {
-    getAllJokes
+    getAllJokes,
+    getJokeAtIndex,
+    getJokeCount
 };
