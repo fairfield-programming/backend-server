292b72d43ace83879c410b9a4c40db9671ceda01
William McGonagle <wpmcgonagle@gmail.com>
2021-12-24 11:24:42 -0500

    🚀 Vulgar Library Almost Done

--- src/library/VulgarTest.js
+++ src/library/VulgarTest.js
@@ -1,5 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+
 function GetVulgarWords() {
 
+    // Get the File Data
+    var fileData = fs.readFileSync(path.join(__dirname, './vulgar.txt'), 'ascii');
+    var wordArray = fileData.split('\n');
 
+    // Return the Words
+    return wordArray;
 
 }
+
+function TokenizeString(input) {
+
+    // Make it Lowercase
+    var lowercase = input.toLowerCase();
+
+    // Switch Around Similar Looking Symbols
+    var partsRemoved = lowercase;
+    partsRemoved = partsRemoved.replace(/\$/g, 's');
+
+    // Return the Final Result
+    return partsRemoved;
+
+}
+
+function DetectVulgarWords(input) {
+
+    // Tokenize the Input String
+    var tokenizedString = TokenizeString(input);
+    var vulgarWords = GetVulgarWords();
+
+    for (var i = 0; i < vulgarWords.length; i++) {
+
+        
+
+    }
+
+    // Return False if Nothing Detected
+    return false;
+
+}
+
+module.exports = {
+    GetVulgarWords,
+    TokenizeString,
+    DetectVulgarWords
+};
