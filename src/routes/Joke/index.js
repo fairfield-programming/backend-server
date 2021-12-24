e21aa83a68caf70cb7be39b7a684b5d33be66d08
William McGonagle <wpmcgonagle@gmail.com>
2021-12-23 21:32:26 -0500

    🔍 Added More Jokes

--- src/routes/Joke/index.js
+++ src/routes/Joke/index.js
@@ -3,7 +3,10 @@ module.exports = (req, res) => {
     // Create an Array of Jokes
     var jokes = [
         "What's a duck's favorite ballet? The Nutquacker.",
-        ""
+        "What's a duck's favorite part of the news? The feather forecast.",
+        "Why did the duck go to the chiropractor? To get its back quacked.",
+        "Where do ducks go when they're sick? The ducktor's office.",
+        "Why do ducks never grow up? Because they grown down."
     ];
 
     // Pick a Joke to Send
