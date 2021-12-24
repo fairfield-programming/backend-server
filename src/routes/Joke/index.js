63cdc31fc035027e2e20f85ce6b0867a010761c4
William McGonagle <wpmcgonagle@gmail.com>
2021-12-23 21:34:51 -0500

    🤡 Added Even More Jokes

--- src/routes/Joke/index.js
+++ src/routes/Joke/index.js
@@ -6,7 +6,10 @@ module.exports = (req, res) => {
         "What's a duck's favorite part of the news? The feather forecast.",
         "Why did the duck go to the chiropractor? To get its back quacked.",
         "Where do ducks go when they're sick? The ducktor's office.",
-        "Why do ducks never grow up? Because they grown down."
+        "Why do ducks never grow up? Because they grown down.",
+        "What do ducks have with their soup? Quackers.",
+        "What do you call a duck that breaks into people's houses? A robber ducky.",
+        "What are duck's favorite zoo animal? Quackodiles."
     ];
 
     // Pick a Joke to Send
