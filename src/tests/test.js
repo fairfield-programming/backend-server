6195c5264dad195331515da0387afa5e754aec02
William McGonagle <wpmcgonagle@gmail.com>
2021-12-23 22:21:46 -0500

    ✅ Finished the Count Test

--- src/tests/test.js
+++ src/tests/test.js
@@ -119,6 +119,47 @@ describe('User Endpoints', () => {
 
     });
 
+    describe('GET /jokes/:id', () => {
+
+        it ('should return a 200 and all the jokes', async () => {
+
+            const res = await requestWithSupertest.get('/jokes/0');
+            
+            // Make Sure None of the Jokes are Funny
+            var funny = false;
+            expect(funny).toBe(false);
+
+            expect(typeof res.text).toBe('string');
+
+            expect(res.status).toEqual(200); 
+            expect(res.type).toEqual(expect.stringContaining('html'));
+
+        });
+
+    });
+
+    describe('GET /jokes/count', () => {
+
+        it ('should return a 200 and the joke count', async () => {
+
+            const res = await requestWithSupertest.get('/jokes/count');
+            
+            // Make Sure None of the Jokes are Funny
+            var funny = false;
+            expect(funny).toBe(false);
+
+            expect(typeof res.text).toBe('string');
+
+            var number = parseInt(res.text, 10);
+            expect(isNaN(number)).toBe(false);
+
+            expect(res.status).toEqual(200); 
+            expect(res.type).toEqual(expect.stringContaining('html'));
+
+        });
+
+    });
+
     describe('GET /jokes/random', () => {
 
         it ('should return a 200 and a joke', async () => {
