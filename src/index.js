8ad130de98ad02319b3e7273e2b671896a37281d
William McGonagle <wpmcgonagle@gmail.com>
2021-12-25 18:34:06 -0500

    🚑 Finished Up Middleware?

--- src/index.js
+++ src/index.js
@@ -19,22 +19,27 @@ app.use(express.json());
 
 // Auth Middleware
 app.use((req, res, next) => {
-	
+
     const authHeader = req.headers.authorization;
 
     if (authHeader) {
         const token = authHeader.split(' ')[1];
 
         jwt.verify(token, accessTokenSecret, (err, user) => {
-            if (err) {
+            
+			if (err) {
                 return res.sendStatus(403);
             }
 
             req.user = user;
             next();
+
         });
+
     } else {
-        res.sendStatus(401);
+        
+		next();
+
     }
 });
 
