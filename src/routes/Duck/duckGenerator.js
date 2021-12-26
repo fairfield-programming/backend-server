275e41a30be8c3f0d9a737e43587e5f98cf7d83d
William McGonagle <wpmcgonagle@gmail.com>
2021-12-26 18:15:14 -0500

    ✨ Added Items into Parser

--- src/routes/Duck/duckGenerator.js
+++ src/routes/Duck/duckGenerator.js
@@ -28,7 +28,7 @@ function parseDuckString(input) {
 function parseDuckV1String(input) {
 
     // Make Sure Length is Right
-    if (input.length != 15) return false;
+    if (input.length != 17) return false;
 
     // Get Duck Hat
     var hatString = input.substring(1, 3);
@@ -54,12 +54,16 @@ function parseDuckV1String(input) {
     var tailString = input.substring(11, 13);
     var tail = parseInt(tailString, 16);
 
+    // Get Duck Item
+    var itemString = input.substring(13, 15);
+    var item = parseInt(itemString, 16);
+
     // Get Beak Color
-    var beakString = input.substring(13, 14);
+    var beakString = input.substring(15, 16);
     var beakColor = parseInt(beakString, 16);
 
     // Get Feather Color
-    var featherString = input.substring(14, 15);
+    var featherString = input.substring(16, 17);
     var featherColor = parseInt(featherString, 16);
 
     // Return the Final Duck Data
@@ -70,6 +74,7 @@ function parseDuckV1String(input) {
         wings: wings,
         smoke: smoke,
         tail: tail,
+        item: item,
         beakColor: beakColor,
         color: featherColor
     };
@@ -126,6 +131,7 @@ function generateDuck (duckData) {
         eyes: duckData.eyes || 0,
         smoke: duckData.smoke || 0,
         tail: duckData.tail || 0,
+        item: duckData.item || 0,
         color: colors[duckData.color] || colors[3],
         beakColor: colors[duckData.beakColor] || colors[5],
     };
@@ -157,83 +163,23 @@ function generateDuck (duckData) {
 
     }
 
-    // Add Smoke 
-    switch (trueDuckData.smoke) {
+    // Add the Smoke (Spread it First)
+    output.push(...getItemString('smoke', trueDuckData.smoke));
 
-        case 0:
-            break;
+    // Add the Eyes (Spread it First)
+    output.push(...getItemString('eyes', trueDuckData.eyes));
 
-        case 1: 
+    // Add the Beak (Spread it First)
+    output.push(...getItemString('beak', trueDuckData.beak));
 
-            output.push(`<path d="M137.087 90.3564L126.838 93.0423L126.415 91.519L136.663 88.833L138 88.5627L139.5 90.0627L142.5 105.563L141 106.063L138 91.0627L137.087 90.3564Z" fill="white" stroke="black"/>`); 
-            output.push(`<path d="M144 96.5627L136.5 98.0627L138.5 113.063L145.5 112.563L144 96.5627Z" stroke="black" stroke-miterlimit="16"/>`);
-            output.push(`<path d="M144.5 103.063L137.367 104.563L138.5 113.063L145.5 112.563L144.5 103.063Z" fill="#4C89E4" stroke="black" stroke-miterlimit="16"/>`);
+    // Add the Items (Spread it First)
+    output.push(...getItemString('items', trueDuckData.items));
 
-            break;
-
-        case 2:
-
-            output.push(`<path d="M139.738 83.1243C139.816 84.0838 142.17 85.3449 143.337 85.8556C143.92 84.7227 144.95 82.2585 144.405 81.465C143.724 80.473 142.265 80.0661 142.071 82.2946C140.515 80.6883 139.64 81.9249 139.738 83.1243Z" fill="#E05050" stroke="black" stroke-miterlimit="2.9238"/>`);
-            output.push(`<path d="M149.937 73.6679C149.132 74.8052 150.571 78.3925 151.392 80.0441C153.092 79.3291 156.525 77.5523 156.658 76.1653C156.824 74.4316 155.583 72.6239 153.297 74.9166C153.063 71.6874 150.943 72.2463 149.937 73.6679Z" fill="#E05050" stroke="black" stroke-miterlimit="2.9238"/>`);
-            output.push(`<path d="M137.102 65.6581C137.432 67.6607 142.596 69.9048 145.137 70.7765C146.167 68.2955 147.906 62.94 146.624 61.3665C145.021 59.3996 141.885 58.7961 141.863 63.5123C138.315 60.4054 136.69 63.1547 137.102 65.6581Z" fill="#E05050" stroke="black" stroke-miterlimit="2.9238"/>`);
-
-            break;
-
-        case 3:
-
-            output.push(`<path d="M139.738 83.1243C139.816 84.0838 142.17 85.3449 143.337 85.8556C143.92 84.7227 144.95 82.2585 144.405 81.465C143.724 80.473 142.265 80.0661 142.071 82.2946C140.515 80.6883 139.64 81.9249 139.738 83.1243Z" fill="#4C89E4" stroke="black" stroke-miterlimit="2.9238"/>`);
-            output.push(`<path d="M149.937 73.6679C149.132 74.8052 150.571 78.3925 151.392 80.0441C153.092 79.3291 156.525 77.5523 156.658 76.1653C156.824 74.4316 155.583 72.6239 153.297 74.9166C153.063 71.6874 150.943 72.2463 149.937 73.6679Z" fill="#4C89E4" stroke="black" stroke-miterlimit="2.9238"/>`);
-            output.push(`<path d="M137.102 65.6581C137.432 67.6607 142.596 69.9048 145.137 70.7765C146.167 68.2955 147.906 62.94 146.624 61.3665C145.021 59.3996 141.885 58.7961 141.863 63.5123C138.315 60.4054 136.69 63.1547 137.102 65.6581Z" fill="#4C89E4" stroke="black" stroke-miterlimit="2.9238"/>`);
-
-            break;
-
-    }
-
-    // Add Eyes
-    switch (trueDuckData.eyes) {
-
-        case 0:
-
-            output.push(`<ellipse cx="114.5" cy="82.5" rx="3.5" ry="5.5" fill="white"/>`);
-            output.push(`<ellipse cx="123.5" cy="79" rx="2.5" ry="4" fill="white"/>`);
-            output.push(`<path d="M115.316 80.5C116.073 81.5182 113.75 82.0151 114.022 84M124.803 77C125.534 78.0956 123.998 78.558 123.941 80.5" stroke="black"/>`);
-        
-            break;
-
-        case 1:
-
-            output.push(`<path d="M121 87.5L109.952 90L109.5 83.5L97.5 78L97 82L95 79.5L97 76L109.952 80.5L131 75L130.5 84.5L126 83.5L124.5 80.5L121 82V87.5Z" fill="black"/>`);
-            output.push(`<path d="M109.952 80.5L131 75L130.5 84.5L126 83.5L124.5 80.5L121 82V87.5L109.952 90L109.5 83.5M109.952 80.5L97 76L95 79.5L97 82L97.5 78L109.5 83.5M109.952 80.5L109.5 83.5" stroke="black"/>`);
-
-            break;
-
-        case 2:
-
-            output.push(`<ellipse cx="114.5" cy="82.5" rx="3.5" ry="5.5" fill="white"/>`);
-            output.push(`<ellipse cx="123.5" cy="79" rx="2.5" ry="4" fill="white"/>`);
-            output.push(`<path d="M115.316 80.5C116.073 81.5182 113.75 82.0151 114.022 84M124.803 77C125.534 78.0956 123.998 78.558 123.941 80.5" stroke="black"/>`);
-            output.push(`<path d="M110.5 77L109.5 74M112.5 76V73.5M115.5 76L116 73.5M121 75.5L119.5 73.5M122.5 74.5L122 72.5M124.5 74.5L125 72.5" stroke="black" stroke-miterlimit="2.9238"/>`);
-
-            break;
-
-        case 3:
-
-            output.push(`<ellipse cx="114.5" cy="82.5" rx="3.5" ry="5.5" fill="white"/>`);
-            output.push(`<ellipse cx="123.5" cy="79" rx="2.5" ry="4" fill="white"/>`);
-            output.push(`<path d="M115.316 80.5C116.073 81.5182 113.75 82.0151 114.022 84M124.803 77C125.534 78.0956 123.998 78.558 123.941 80.5" stroke="black"/>`);
-            output.push(`<path d="M119.5 78L110.5 77.5L110 75L119.5 76L124 72L124.5 74.5L119.5 78Z" fill="black" stroke="black" stroke-miterlimit="2.9238"/>`);
-
-            break;
-
-    }
+    // Add the Wings (Spread it First)
+    output.push(...getItemString('wing', trueDuckData.wings));
     
-    // Add Beak
-    output.push(`<path d="M124.187 83.3262C120.512 81.354 117.645 88.8045 115 92.7763C119.037 96.3372 126.108 99.1037 130.451 94.8307C134.793 90.5576 127.388 93.2817 125.022 92.3654C126.201 92.2563 129.996 89.7431 131.286 89.4893C135.462 88.6676 133.791 84.148 132.956 84.5589C132.121 84.9697 128.78 85.7915 124.187 83.3262Z" fill="${trueDuckData.beakColor}"/>`);
-    output.push(`<path d="M120.846 91.1328C121.135 91.4168 122.384 92.6097 125.022 92.3654M125.022 92.3654C126.201 92.2563 129.996 89.7431 131.286 89.4893C135.462 88.6676 133.791 84.148 132.956 84.5589C132.121 84.9697 128.78 85.7915 124.187 83.3262C120.512 81.354 117.645 88.8045 115 92.7763C119.037 96.3372 126.108 99.1037 130.451 94.8307C134.793 90.5576 127.388 93.2817 125.022 92.3654Z" stroke="black"/>`);
-
-    // Add Wing
-    output.push(`<path d="M85.8869 107.197C99.2754 105.536 100.718 113.707 99.7652 118L76.4987 115.507C71.6004 100.965 80.1723 98.8878 81.3969 102.627C82.3765 105.619 84.7984 106.92 85.8869 107.197Z" fill="${trueDuckData.color}"/>`);
-    output.push(`<path d="M99.7652 118C100.718 113.707 99.2754 105.536 85.8869 107.197C84.7984 106.92 82.3765 105.619 81.3969 102.627C80.1723 98.8878 71.6004 100.965 76.4987 115.507" stroke="black"/>`);
+    // Add the Hat (Spread it First)
+    output.push(...getItemString('hat', trueDuckData.hat));
 
     return output.join("\n");
 
