d94841b43b9cde87862c0c106b700eb960ee6d5a
William McGonagle <wpmcgonagle@gmail.com>
2021-12-26 18:23:05 -0500

    🔒️ Set Headers After Fail Check

--- src/routes/Duck/id.js
+++ src/routes/Duck/id.js
@@ -2,13 +2,13 @@ duckGenerator = require('./duckGenerator');
 
 module.exports = (req, res) => {
 
-    // Set the Headers
-    res.set("Content-Type", "image/svg+xml");
-
     // Parse the String
     var duckData = duckGenerator.parseDuckString(req.params.id);
     if (duckData == false) return res.status(400).send("Bad Request.");
-    
+
+    // Set the Headers
+    res.set("Content-Type", "image/svg+xml");
+        
     // Send the Duck
     return res.send(
         duckGenerator.formatSVG(
--- src/routes/Duck/idZoom.js
+++ src/routes/Duck/idZoom.js
@@ -2,9 +2,6 @@ duckGenerator = require('./duckGenerator');
 
 module.exports = (req, res) => {
 
-    // Set the Headers
-    res.set("Content-Type", "image/svg+xml");
-    
     // Get the Zoom
     var zoom = req.params.zoom;
     var zoomInt = parseInt(zoom);
@@ -12,6 +9,9 @@ module.exports = (req, res) => {
     // Parse the String
     var duckData = duckGenerator.parseDuckString(req.params.id);
     if (duckData == false) return res.status(400).send("Bad Request.");
+    
+    // Set the Headers
+    res.set("Content-Type", "image/svg+xml");
     
     // Send the Duck
     return res.send(
