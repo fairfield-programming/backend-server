e760504f7dc5a9f9bca9bb901708445a896b7d5a
William McGonagle <wpmcgonagle@gmail.com>
2021-12-28 19:45:22 -0500

    ✨ Added Auth to Account Deletion

--- src/routes/User/deleteAccount.js
+++ src/routes/User/deleteAccount.js
@@ -2,6 +2,9 @@ module.exports = (req, res) => {
 
 	if (req.params.id == undefined) return res.status(400).send("Not All Parameters Given.");
 
+	if (req.user == undefined) return res.status(403).send("Not Logged In.");
+	if (req.user.id != req.params.id) return res.status(401).send("Not Authorized.");
+
     User.findOne({
 		where: {
 			id: req.params.id
