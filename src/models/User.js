a45778c4e8f794c4862650b9a4cf2e8156af2831
William-McGonagle <William-McGonagle@users.noreply.github.com>
2021-12-29 20:39:49 +0000

    💅 Prettified Code

--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,10 @@
-module.exports = function(sequelize, DataTypes)
-{
-    return sequelize.define("User",
-    {
-        username: DataTypes.STRING,
-        password: DataTypes.TEXT,
-        email: DataTypes.TEXT,
-        biography: DataTypes.TEXT,
-        profilePicture: DataTypes.TEXT,
-        status: DataTypes.STRING,
-    });
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define("User", {
+    username: DataTypes.STRING,
+    password: DataTypes.TEXT,
+    email: DataTypes.TEXT,
+    biography: DataTypes.TEXT,
+    profilePicture: DataTypes.TEXT,
+    status: DataTypes.STRING,
+  });
 };
--- src/routes/User/Account/setData.js
+++ src/routes/User/Account/setData.js
@@ -1,61 +1,54 @@
 const vulgarTester = require("../../../library/VulgarTest");
 const { notEnoughParametersForSetData } = require("../../../library/validator");
 
-module.exports = (req, res) =>
-{
-    if (req.user == undefined) return res.status(403).send("Not Logged In.");
+module.exports = (req, res) => {
+  if (req.user == undefined) return res.status(403).send("Not Logged In.");
 
-    if (propertyNotFound(req.body.biography)) return res.status(400).send("Not All Parameters Given.");
-    if (propertyNotFound(req.body.profilePicture)) return res.status(400).send("Not All Parameters Given.");
-    if (propertyNotFound(req.body.username)) return res.status(400).send("Not All Parameters Given.");
+  if (propertyNotFound(req.body.biography))
+    return res.status(400).send("Not All Parameters Given.");
+  if (propertyNotFound(req.body.profilePicture))
+    return res.status(400).send("Not All Parameters Given.");
+  if (propertyNotFound(req.body.username))
+    return res.status(400).send("Not All Parameters Given.");
 
-    User.findOne(
-        {
-            where:
-            {
-                id: req.user.id,
-            },
+  User.findOne({
+    where: {
+      id: req.user.id,
+    },
+  })
+    .then(function (data) {
+      if (data == null) return res.status(404).send("Not Found.");
+
+      if (vulgarWordsFound(req))
+        return res.status(406).send("Vulgar Language Detected.");
+
+      data
+        .update({
+          biography: req.body.biography || data.biography,
+          profilePicture: req.body.profilePicture || data.profilePicture,
+          username: req.body.username || data.username,
         })
-        .then(function(data)
-        {
-            if (data == null) return res.status(404).send("Not Found.");
-
-            if (vulgarWordsFound(req))
-                return res.status(406).send("Vulgar Language Detected.");
-
-            data
-                .update(
-                {
-                    biography: req.body.biography || data.biography,
-                    profilePicture: req.body.profilePicture || data.profilePicture,
-                    username: req.body.username || data.username,
-                })
-                .then(function(newData)
-                {
-                    return res.status(200).json(
-                    {
-                        id: newData.id,
-                        username: newData.username,
-                        biography: newData.biography,
-                        profilePicture: newData.profilePicture,
-                    });
-                })
-                .catch(function(error)
-                {
-                    console.log(error);
-                    return res.status(500).send("Internal Server Error.");
-                });
+        .then(function (newData) {
+          return res.status(200).json({
+            id: newData.id,
+            username: newData.username,
+            biography: newData.biography,
+            profilePicture: newData.profilePicture,
+          });
         })
-        .catch(function(error)
-        {
-            console.log(error);
-            return res.status(500).send("Internal Server Error.");
+        .catch(function (error) {
+          console.log(error);
+          return res.status(500).send("Internal Server Error.");
         });
+    })
+    .catch(function (error) {
+      console.log(error);
+      return res.status(500).send("Internal Server Error.");
+    });
 };
 
-function vulgarWordsFound(req)
-{
-    if (vulgarTester.DetectVulgarWords(req.body.biography)) return true;
-    if (vulgarTester.DetectVulgarWords(req.body.username)) return true;
-    return false;
+function vulgarWordsFound(req) {
+  if (vulgarTester.DetectVulgarWords(req.body.biography)) return true;
+  if (vulgarTester.DetectVulgarWords(req.body.username)) return true;
+  return false;
 }
