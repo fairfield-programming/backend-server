786145dbabbccb5974e6d12dfad7fd312480ef4d
William-McGonagle <William-McGonagle@users.noreply.github.com>
2021-12-29 21:04:20 +0000

    💅 Prettified Code

--- src/library/validator.js
+++ src/library/validator.js
@@ -1,44 +1,40 @@
 // Check if Undefined
-function propertyNotFound(property)
-{
-    if (typeof property === "undefined") return true;
-    if (property == undefined) return true;
-    return false;
+function propertyNotFound(property) {
+  if (typeof property === "undefined") return true;
+  if (property == undefined) return true;
+  return false;
 }
 
-function invalidPassword(password)
-{
-    if (password.length > 14) return true;
-    if (password.length < 4) return true;
-    if (!password.match(/[A-Z]/)) return true;
-    if (!password.match(/[a-z]/)) return true;
-    if (!password.match(/\d/)) return true;
-    if (!password.match(/[\\#?!@$%^&*\-_]/)) return true;
-    return false;
+function invalidPassword(password) {
+  if (password.length > 14) return true;
+  if (password.length < 4) return true;
+  if (!password.match(/[A-Z]/)) return true;
+  if (!password.match(/[a-z]/)) return true;
+  if (!password.match(/\d/)) return true;
+  if (!password.match(/[\\#?!@$%^&*\-_]/)) return true;
+  return false;
 }
 
-function invalidUsername(username)
-{
-    if (username.length > 30) return true;
-    if (username.length < 4) return true;
-    if (username.includes(" ")) return true;
-    if (/\d/g.test(username)) return true;
-    if (/[!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/g.test(username)) return true;
-    if (username.toLowerCase() !== username) return true;
-    return false;
+function invalidUsername(username) {
+  if (username.length > 30) return true;
+  if (username.length < 4) return true;
+  if (username.includes(" ")) return true;
+  if (/\d/g.test(username)) return true;
+  if (/[!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/g.test(username)) return true;
+  if (username.toLowerCase() !== username) return true;
+  return false;
 }
 
-function invalidEmail(email)
-{
-    if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) return false;
-    return true;
+function invalidEmail(email) {
+  if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) return false;
+  return true;
 }
 
 module.exports = {
-    notEnoughParametersForSetData,
-    notEnoughParametersForSignup,
-    propertyNotFound,
-    invalidPassword,
-    invalidUsername,
-    invalidEmail,
+  notEnoughParametersForSetData,
+  notEnoughParametersForSignup,
+  propertyNotFound,
+  invalidPassword,
+  invalidUsername,
+  invalidEmail,
 };
