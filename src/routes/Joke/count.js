3a82206cbb0615d353c8f0d0af08e3811fbdd884
William-McGonagle <William-McGonagle@users.noreply.github.com>
2021-12-29 20:36:39 +0000

    💅 Prettified Code

--- src/routes/Joke/count.js
+++ src/routes/Joke/count.js
@@ -1,10 +1,9 @@
 const jokeLib = require("./jokeFinder");
 
-module.exports = (req, res) =>
-{
-    // Set the Access-Control-Allow-Origin Header
-    res.set("Access-Control-Allow-Origin", "*");
+module.exports = (req, res) => {
+  // Set the Access-Control-Allow-Origin Header
+  res.set("Access-Control-Allow-Origin", "*");
 
-    // Return the Count
-    return res.send(jokeLib.getJokeCount().toString());
+  // Return the Count
+  return res.send(jokeLib.getJokeCount().toString());
 };
