00e8d4fc1c23f192f8392bd00e0fa43f841f6c98
William-McGonagle <William-McGonagle@users.noreply.github.com>
2021-12-29 21:06:55 +0000

    💅 Prettified Code

--- src/routes/Duck/index.js
+++ src/routes/Duck/index.js
@@ -1,11 +1,9 @@
 const duckGenerator = require("./duckGenerator");
 
-module.exports = (req, res) =>
-{
-    // Set the Headers
-    res.set("Content-Type", "image/svg+xml");
+module.exports = (req, res) => {
+  // Set the Headers
+  res.set("Content-Type", "image/svg+xml");
 
-    // Send the Duck
-    res.send(duckGenerator.formatSVG(duckGenerator.generateDuck(
-    {})));
+  // Send the Duck
+  res.send(duckGenerator.formatSVG(duckGenerator.generateDuck({})));
 };
