c5f839165ae6d6eae923414935a7da66a7da4817
NeilChaudhari <NeilChaudhari@users.noreply.github.com>
2021-12-30 02:41:16 +0000

    💅 Prettified Code

--- src/routes/User/Followers/followUser.js
+++ src/routes/User/Followers/followUser.js
@@ -1,48 +1,37 @@
-module.exports = (req, res) =>
-{
-    if (req.user == undefined) return res.status(403).send("Not Logged In.");
-    if (req.params.id || req.params.followerId == undefined)
-        return res.status(400).send("Not All Parameters Provided.");
+module.exports = (req, res) => {
+  if (req.user == undefined) return res.status(403).send("Not Logged In.");
+  if (req.params.id || req.params.followerId == undefined)
+    return res.status(400).send("Not All Parameters Provided.");
 
-    User.findOne(
-        {
-            where:
-            {
-                id: req.user.id,
-            },
+  User.findOne({
+    where: {
+      id: req.user.id,
+    },
+  })
+    .then(function (followerData) {
+      User.findOne({
+        where: {
+          id: req.params.followerId,
+        },
+      })
+        .then(function (followeeData) {
+          followeeData
+            .addFollower(followerData)
+            .then(function (success) {
+              return res.json(followeeData);
+            })
+            .catch(function () {
+              console.log(error);
+              return res.status(500).send("Internal Server Error.");
+            });
         })
-        .then(function(followerData)
-        {
-            User.findOne(
-                {
-                    where:
-                    {
-                        id: req.params.followerId,
-                    },
-                })
-                .then(function(followeeData)
-                {
-                    followeeData
-                        .addFollower(followerData)
-                        .then(function(success)
-                        {
-                            return res.json(followeeData);
-                        })
-                        .catch(function()
-                        {
-                            console.log(error);
-                            return res.status(500).send("Internal Server Error.");
-                        });
-                })
-                .catch(function()
-                {
-                    console.log(error);
-                    return res.status(500).send("Internal Server Error.");
-                });
-        })
-        .catch(function()
-        {
-            console.log(error);
-            return res.status(500).send("Internal Server Error.");
+        .catch(function () {
+          console.log(error);
+          return res.status(500).send("Internal Server Error.");
         });
+    })
+    .catch(function () {
+      console.log(error);
+      return res.status(500).send("Internal Server Error.");
+    });
 };
--- src/routes/User/Followers/unfollowUser.js
+++ src/routes/User/Followers/unfollowUser.js
@@ -1,52 +1,40 @@
-module.exports = (req, res) =>
-{
-    if (req.user == undefined) return res.status(403).send("Not Logged In.");
-    if (req.params.id || req.params.followerId == undefined)
-        return res.status(400).send("Not All Parameters Provided.");
+module.exports = (req, res) => {
+  if (req.user == undefined) return res.status(403).send("Not Logged In.");
+  if (req.params.id || req.params.followerId == undefined)
+    return res.status(400).send("Not All Parameters Provided.");
 
-    User.findOne(
-        {
-            where:
-            {
-                id: req.params.followerId,
-            },
+  User.findOne({
+    where: {
+      id: req.params.followerId,
+    },
+  })
+    .then(function (followeeData) {
+      User.findOne({
+        where: {
+          id: req.user.id,
+        },
+      })
+        .then(function (followerData) {
+          if (!followerData.hasFollower(followeeData)) {
+            return res.status(401).send("You do not follow this person");
+          }
+          followeeData
+            .removeFollower(followerData)
+            .then(function (success) {
+              return res.json(followeeData);
+            })
+            .catch(function () {
+              console.log(error);
+              return res.status(500).send("Internal Server Error.");
+            });
         })
-        .then(function(followeeData)
-        {
-            User.findOne(
-                {
-                    where:
-                    {
-                        id: req.user.id,
-                    },
-                })
-                .then(function(followerData)
-                {
-                    if (!followerData.hasFollower(followeeData))
-                    {
-                        return res.status(401).send("You do not follow this person");
-                    }
-                    followeeData
-                        .removeFollower(followerData)
-                        .then(function(success)
-                        {
-                            return res.json(followeeData);
-                        })
-                        .catch(function()
-                        {
-                            console.log(error);
-                            return res.status(500).send("Internal Server Error.");
-                        });
-                })
-                .catch(function()
-                {
-                    console.log(error);
-                    return res.status(500).send("Internal Server Error.");
-                });
-        })
-        .catch(function()
-        {
-            console.log(error);
-            return res.status(500).send("Internal Server Error.");
+        .catch(function () {
+          console.log(error);
+          return res.status(500).send("Internal Server Error.");
         });
+    })
+    .catch(function () {
+      console.log(error);
+      return res.status(500).send("Internal Server Error.");
+    });
 };
