0a96b5b3c5bd5188f039ea7521fa9a35e6f4ee63
William-McGonagle <William-McGonagle@users.noreply.github.com>
2021-12-30 18:36:57 +0000

    💅 Prettified Code

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,5 @@
 // Import Sequelize
-const
-{
-    Sequelize,
-    Model,
-    DataTypes,
-    Op
-} = require("sequelize");
+const { Sequelize, Model, DataTypes, Op } = require("sequelize");
 
 // Setup Global Operator
 global.Op = Op;
@@ -17,27 +11,20 @@ var env = process.env.NODE_ENV || "development";
 // Second Check
 if (process.env.DATABASE_URL == undefined) env = "development";
 
-if (env == "development")
-{
-    sequelize = new Sequelize("sqlite::memory:",
-    {
-        logging: false,
-    });
-}
-else
-{
-    sequelize = new Sequelize(process.env.DATABASE_URL,
-    {
-        logging: false,
-        dialectOptions:
-        {
-            ssl:
-            {
-                require: true,
-                rejectUnauthorized: false,
-            },
-        },
-    });
+if (env == "development") {
+  sequelize = new Sequelize("sqlite::memory:", {
+    logging: false,
+  });
+} else {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
 }
 
 // Import Models
@@ -53,41 +40,35 @@ global.Events = Events(sequelize, DataTypes);
 // Setup Relationships
 
 // Relationship for Events
-global.User.belongsToMany(global.Events,
-{
-    through: "EventSubscribers",
+global.User.belongsToMany(global.Events, {
+  through: "EventSubscribers",
 });
-global.Events.belongsToMany(global.User,
-{
-    through: "EventSubscribers",
+global.Events.belongsToMany(global.User, {
+  through: "EventSubscribers",
 });
 
 // Relationship for Follower System
-global.User.belongsToMany(global.User,
-{
-    through: "Followers",
-    as: "Followee",
-    foreignKey: "followeeId",
+global.User.belongsToMany(global.User, {
+  through: "Followers",
+  as: "Followee",
+  foreignKey: "followeeId",
 });
-global.User.belongsToMany(global.User,
-{
-    through: "Followers",
-    as: "Follower",
-    foreignKey: "followerId",
+global.User.belongsToMany(global.User, {
+  through: "Followers",
+  as: "Follower",
+  foreignKey: "followerId",
 });
 
 // Relationship for Block System
-global.User.belongsToMany(global.User,
-{
-    through: "Blocked",
-    as: "Blocker",
-    foreignKey: "blockerId",
+global.User.belongsToMany(global.User, {
+  through: "Blocked",
+  as: "Blocker",
+  foreignKey: "blockerId",
 });
-global.User.belongsToMany(global.User,
-{
-    through: "Blocked",
-    as: "Blocked",
-    foreignKey: "blockedId",
+global.User.belongsToMany(global.User, {
+  through: "Blocked",
+  as: "Blocked",
+  foreignKey: "blockedId",
 });
 
 global.sequelize = sequelize;
