07f1b268b2221b5f4b5c65ad582b9e0212bb8acc
NikitaLejnev <catassasin331@gmail.com>
2021-12-31 21:02:01 +0300

    Linting

--- src/tests/joke.js
+++ src/tests/joke.js
@@ -1,65 +1,56 @@
-const server = require("../index.js");
-const supertest = require("supertest");
+const supertest = require('supertest');
+const server = require('../index');
+
 const requestWithSupertest = supertest(server);
 
-describe("GET /jokes", () =>
-{
-    it("should return a 200 and all the jokes", async () =>
-    {
-        const res = await requestWithSupertest.get("/jokes");
+describe('GET /jokes', () => {
+  it('should return a 200 and all the jokes', async () => {
+    const res = await requestWithSupertest.get('/jokes');
 
-        expect(typeof res.body).toBe("object");
-        expect(Array.isArray(res.body)).toBe(true);
-        expect(res.status).toEqual(200);
-        expect(res.type).toEqual(expect.stringContaining("json"));
-    });
+    expect(typeof res.body).toBe('object');
+    expect(Array.isArray(res.body)).toBe(true);
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual(expect.stringContaining('json'));
+  });
 });
 
-describe("GET /jokes/:id", () =>
-{
-    it("should return a 200 and all the jokes", async () =>
-    {
-        const res = await requestWithSupertest.get("/jokes/0");
-        expect(res.type).toEqual(expect.stringContaining("html"));
-    });
+describe('GET /jokes/:id', () => {
+  it('should return a 200 and all the jokes', async () => {
+    const res = await requestWithSupertest.get('/jokes/0');
+    expect(res.type).toEqual(expect.stringContaining('html'));
+  });
 });
 
-describe("GET /jokes/count", () =>
-{
-    it("should return a 200 and the joke count", async () =>
-    {
-        const res = await requestWithSupertest.get("/jokes/count");
+describe('GET /jokes/count', () => {
+  it('should return a 200 and the joke count', async () => {
+    const res = await requestWithSupertest.get('/jokes/count');
 
-        expect(typeof res.text).toBe("string");
+    expect(typeof res.text).toBe('string');
 
-        const number = parseInt(res.text, 10);
-        expect(isNaN(number)).toBe(false);
+    const number = parseInt(res.text, 10);
+    expect(typeof number !== 'number').toBe(false);
 
-        expect(res.status).toEqual(200);
-        expect(res.type).toEqual(expect.stringContaining("html"));
-    });
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual(expect.stringContaining('html'));
+  });
 });
 
-describe("GET /jokes/random", () =>
-{
-    it("should return a 200 and a joke", async () =>
-    {
-        const res = await requestWithSupertest.get("/jokes/random");
+describe('GET /jokes/random', () => {
+  it('should return a 200 and a joke', async () => {
+    const res = await requestWithSupertest.get('/jokes/random');
 
-        expect(typeof res.text).toBe("string");
-        expect(res.status).toEqual(200);
-        expect(res.type).toEqual(expect.stringContaining("html"));
-    });
+    expect(typeof res.text).toBe('string');
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual(expect.stringContaining('html'));
+  });
 });
 
-describe("GET /joke", () =>
-{
-    it("should return a 200 and a joke", async () =>
-    {
-        const res = await requestWithSupertest.get("/joke");
+describe('GET /joke', () => {
+  it('should return a 200 and a joke', async () => {
+    const res = await requestWithSupertest.get('/joke');
 
-        expect(typeof res.text).toBe("string");
-        expect(res.status).toEqual(200);
-        expect(res.type).toEqual(expect.stringContaining("html"));
-    });
+    expect(typeof res.text).toBe('string');
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual(expect.stringContaining('html'));
+  });
 });
