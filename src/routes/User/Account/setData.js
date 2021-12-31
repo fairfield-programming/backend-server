6bd0ea7f11609fbf58906b79f2db96373bc77185
NikitaLejnev <catassasin331@gmail.com>
2021-12-31 23:43:30 +0300

    Linting

--- src/routes/User/Account/setData.js
+++ src/routes/User/Account/setData.js
@@ -1,55 +1,50 @@
 const vulgarTester = require("../../../library/VulgarTest");
 
-module.exports = (req, res) =>
-{
-    if (!req.user) return res.status(403).send("Not Logged In.");
+module.exports = (req, res) => {
+  if (!req.user) return res.status(403).send("Not Logged In.");
 
-    const { biography, profilePicture, username } = req.body;
-    if (!biography || !profilePicture || !username)
-        return res.status(400).send("Not All Parameters Given.");
+  const { biography, profilePicture, username } = req.body;
+  if (!biography || !profilePicture || !username)
+    return res.status(400).send("Not All Parameters Given.");
 
-    User.findOne(
-        {
-            where:
+  User.findOne(
+    {
+      where:
+      {
+        id: req.user.id,
+      },
+    })
+    .then(function (data) {
+      if (!data) return res.status(404).send("Not Found.");
+
+      if (vulgarTester.DetectVulgarWords(req.body.biography))
+        return res.status(406).send("Vulgar Language Detected.");
+      if (vulgarTester.DetectVulgarWords(req.body.username))
+        return res.status(406).send("Vulgar Language Detected.");
+
+      data
+        .update(
+          {
+            biography: req.body.biography || data.biography,
+            profilePicture: req.body.profilePicture || data.profilePicture,
+            username: req.body.username || data.username,
+          })
+        .then(function (newData) {
+          return res.status(200).json(
             {
-                id: req.user.id,
-            },
+              id: newData.id,
+              username: newData.username,
+              biography: newData.biography,
+              profilePicture: newData.profilePicture,
+            });
         })
-        .then(function(data)
-        {
-            if (!data) return res.status(404).send("Not Found.");
-
-            if (vulgarTester.DetectVulgarWords(req.body.biography))
-                return res.status(406).send("Vulgar Language Detected.");
-            if (vulgarTester.DetectVulgarWords(req.body.username))
-                return res.status(406).send("Vulgar Language Detected.");
-
-            data
-                .update(
-                {
-                    biography: req.body.biography || data.biography,
-                    profilePicture: req.body.profilePicture || data.profilePicture,
-                    username: req.body.username || data.username,
-                })
-                .then(function(newData)
-                {
-                    return res.status(200).json(
-                    {
-                        id: newData.id,
-                        username: newData.username,
-                        biography: newData.biography,
-                        profilePicture: newData.profilePicture,
-                    });
-                })
-                .catch(function(error)
-                {
-                    console.log(error);
-                    return res.status(500).send("Internal Server Error.");
-                });
-        })
-        .catch(function(error)
-        {
-            console.log(error);
-            return res.status(500).send("Internal Server Error.");
+        .catch(function (error) {
+          console.log(error);
+          return res.status(500).send("Internal Server Error.");
         });
+    })
+    .catch(function (error) {
+      console.log(error);
+      return res.status(500).send("Internal Server Error.");
+    });
 };
--- src/routes/User/Account/signup.js
+++ src/routes/User/Account/signup.js
@@ -1,88 +1,72 @@
 const
-{
+  {
     invalidPassword,
     invalidUsername,
     invalidEmail,
-} = require("../../../library/validator");
+  } = require("../../../library/validator");
 
-module.exports = (req, res) =>
-{
-    const { username, email, password } = req.body;
-    // Make sure all Params Exist
-    if (!username || !email || !password)
-        return res.status(400).send("Not All Parameters Provided.");
+module.exports = (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) return res.status(400).send("Not All Parameters Provided.");
 
-    // Check Password, Email and Username
-    if (invalidPassword(req.body.password))
-        return res.status(400).send("Invalid Password.");
-    if (invalidEmail(req.body.email))
-        return res.status(400).send("Invalid Email.");
-    if (invalidUsername(req.body.username))
-        return res.status(400).send("Invalid Username.");
+  if (invalidPassword(req.body.password)) return res.status(400).send("Invalid Password.");
+  if (invalidEmail(req.body.email)) return res.status(400).send("Invalid Email.");
+  if (invalidUsername(req.body.username)) return res.status(400).send("Invalid Username.");
 
-    // Find All Users with Similar Usernames and Emails
-    User.findAll(
-        {
-            where:
+  // Find All Users with Similar Usernames and Emails
+  User.findAll(
+    {
+      where:
+      {
+        [Op.or]: [
+          {
+            username: req.body.username,
+          },
+          {
+            email: req.body.email,
+          },],
+      },
+    })
+    .then((userData) => {
+      // If Similar Accounts Exist, Don't Let Them Create an Account
+      if (userData.length > 0) return res.status(403).send("Account Already Exists.");
+
+      // Hash the Password
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).send("Internal Server Error.");
+        }
+
+        // Create the New Account
+        User.create(
+          {
+            username: req.body.username,
+            password: hash,
+            email: req.body.email,
+          })
+          .then((data) => res.json(
             {
-                [Op.or]: [
+              token: jwt.sign(
                 {
-                    username: req.body.username,
+                  id: userData.id,
+                  username: data.username,
+                  email: data.email,
                 },
-                {
-                    email: req.body.email,
-                }, ],
+                process.env.JWT_KEY,
+              ),
             },
-        })
-        .then(function(userData)
-        {
-            // If Similar Accounts Exist, Don't Let Them Create an Account
-            if (userData.length > 0)
-                return res.status(403).send("Account Already Exists.");
-
-            // Hash the Password
-            bcrypt.hash(req.body.password, 10, function(err, hash)
-            {
-                if (err)
-                {
-                    console.log(err);
-                    return res.status(500).send("Internal Server Error.");
-                }
-
-                // Create the New Account
-                User.create(
-                    {
-                        username: req.body.username,
-                        password: hash,
-                        email: req.body.email,
-                    })
-                    .then(function(data)
-                    {
-                        // Return the Token from the New Account
-                        return res.json(
-                        {
-                            token: jwt.sign(
-                                {
-                                    id: userData.id,
-                                    username: data.username,
-                                    email: data.email,
-                                },
-                                process.env.JWT_KEY
-                            ),
-                        });
-                    })
-                    .catch(function(error)
-                    {
-                        console.log(error);
-                        return res.status(500).send("Internal Server Error.");
-                    });
-            });
-        })
-        .catch(function(error)
-        {
-            /* istanbul ignore next */
+          ))
+          .catch((error) => {
             console.log(error);
-            /* istanbul ignore next */
             return res.status(500).send("Internal Server Error.");
-        });
+          });
+      });
+    })
+    .catch((error) => {
+      /* istanbul ignore next */
+      console.log(error);
+      /* istanbul ignore next */
+      return res.status(500).send("Internal Server Error.");
+    });
 };
