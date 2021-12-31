c3556ae072fca55935fe4167f2bc5f7bc2243a6f
NikitaLejnev <catassasin331@gmail.com>
2021-12-31 20:24:08 +0300

    Linting

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-require("dotenv").config();
+require('dotenv').config();
 
 // Configure Imports
-const express = require("express");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const
 {
     Sequelize
-} = require("sequelize");
-const models = require("./models");
+} = require('sequelize');
+const models = require('./models');
 
 // Configure Local Variables
 const app = express();
@@ -17,7 +17,7 @@ global.bcrypt = bcrypt;
 global.jwt = jwt;
 
 // Configure Middleware
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.json());
 
 // Auth Middleware
@@ -27,7 +27,7 @@ app.use((req, res, next) =>
 
     if (authHeader)
     {
-        const token = authHeader.split(" ")[1];
+        const token = authHeader.split(' ')[1];
 
         jwt.verify(token, process.env.JWT_KEY, (err, user) =>
         {
@@ -47,77 +47,77 @@ app.use((req, res, next) =>
 });
 
 // Duck Joke Endpoints
-app.get("/joke", require("./routes/Joke/random"));
-app.get("/jokes", require("./routes/Joke/all"));
-app.get("/jokes/count", require("./routes/Joke/count"));
-app.get("/jokes/random", require("./routes/Joke/random"));
-app.get("/jokes/:id", require("./routes/Joke/id"));
+app.get('/joke', require('./routes/Joke/random'));
+app.get('/jokes', require('./routes/Joke/all'));
+app.get('/jokes/count', require('./routes/Joke/count'));
+app.get('/jokes/random', require('./routes/Joke/random'));
+app.get('/jokes/:id', require('./routes/Joke/id'));
 
 // Duck Endpoints
-app.get("/duck", require("./routes/Duck/index"));
-app.get("/duck/:id", require("./routes/Duck/id"));
-app.get("/duck/:id/:zoom", require("./routes/Duck/idZoom"));
+app.get('/duck', require('./routes/Duck/index'));
+app.get('/duck/:id', require('./routes/Duck/id'));
+app.get('/duck/:id/:zoom', require('./routes/Duck/idZoom'));
 
 // Article Endpoints
-app.get("/article/create", require("./routes/Article/createArticle"));
-app.get("/article/:id/", require("./routes/Article/queryArticle"));
-app.get("/article/", require("./routes/Article/listArticles"));
+app.get('/article/create', require('./routes/Article/createArticle'));
+app.get('/article/:id/', require('./routes/Article/queryArticle'));
+app.get('/article/', require('./routes/Article/listArticles'));
 
 // User Endpoints
-app.get("/user/:id/", require("./routes/User/queryUser"));
-app.get("/user/:id/status", require("./routes/User/Account/getStatus"));
-app.get("/user/", require("./routes/User/listUsers"));
+app.get('/user/:id/', require('./routes/User/queryUser'));
+app.get('/user/:id/status', require('./routes/User/Account/getStatus'));
+app.get('/user/', require('./routes/User/listUsers'));
 app.get(
-    "/user/:id/followers",
-    require("./routes/User/Followers/listFollowers")
+    '/user/:id/followers',
+    require('./routes/User/Followers/listFollowers')
 );
 app.get(
-    "/user/:id/followers/:followerId",
-    require("./routes/User/Followers/queryFollower")
+    '/user/:id/followers/:followerId',
+    require('./routes/User/Followers/queryFollower')
 );
 
-app.post("/user/signup", require("./routes/User/Account/signup"));
-app.post("/user/login", require("./routes/User/Account/login"));
-app.post("/user/:id/update", require("./routes/User/Account/setData"));
-app.post("/user/:id/status", require("./routes/User/Account/setStatus"));
-app.post("/user/:id/password", require("./routes/User/Account/setPass"));
-app.post("/user/:id/delete", require("./routes/User/Account/deleteAccount"));
+app.post('/user/signup', require('./routes/User/Account/signup'));
+app.post('/user/login', require('./routes/User/Account/login'));
+app.post('/user/:id/update', require('./routes/User/Account/setData'));
+app.post('/user/:id/status', require('./routes/User/Account/setStatus'));
+app.post('/user/:id/password', require('./routes/User/Account/setPass'));
+app.post('/user/:id/delete', require('./routes/User/Account/deleteAccount'));
 app.post(
-    "/user/:id/follow/:followerId",
-    require("./routes/User/Followers/followUser")
+    '/user/:id/follow/:followerId',
+    require('./routes/User/Followers/followUser')
 );
 app.post(
-    "/user/:id/unfollow/:followerId",
-    require("./routes/User/Followers/unfollowUser")
+    '/user/:id/unfollow/:followerId',
+    require('./routes/User/Followers/unfollowUser')
 );
 app.post(
-    "/useruser/:id/block/:blockId",
-    require("./routes/User/Block/blockUser")
+    '/useruser/:id/block/:blockId',
+    require('./routes/User/Block/blockUser')
 );
 
 //Event Endpoints
-app.get("/event/", require("./routes/Events/listEvents"));
-app.get("/event/:id/", require("./routes/Events/queryEvent"));
+app.get('/event/', require('./routes/Events/listEvents'));
+app.get('/event/:id/', require('./routes/Events/queryEvent'));
 
-app.post("/event/create", require("./routes/Events/createEvent"));
-app.post("/event/:id/delete", require("./routes/Events/deleteEvent"));
-app.post("/event/:id/edit", require("./routes/Events/editEvent"));
-app.post("/event/:id/rsvp", require("./routes/Events/rsvpEvent"));
-app.post("/event/:id/unrsvp", require("./routes/Events/unrsvpEvent"));
+app.post('/event/create', require('./routes/Events/createEvent'));
+app.post('/event/:id/delete', require('./routes/Events/deleteEvent'));
+app.post('/event/:id/edit', require('./routes/Events/editEvent'));
+app.post('/event/:id/rsvp', require('./routes/Events/rsvpEvent'));
+app.post('/event/:id/unrsvp', require('./routes/Events/unrsvpEvent'));
 
 // Sync the Database
 sequelize.sync().then(() =>
 {
-    app.emit("database-started");
+    app.emit('database-started');
 });
 
 // Start Server
-if (process.env.NODE_ENV !== "test")
+if (process.env.NODE_ENV !== 'test')
 {
     app.listen(port, function()
     {
         // Log Server Port to the Console.
-        console.log("Server Listening at http://localhost:" + port);
+        console.log('Server Listening at http://localhost:' + port);
     });
 }
 
