a961315efa57cd01ab6533cb37e4ad94ed2c246c
NikitaLejnev <catassasin331@gmail.com>
2022-01-01 16:44:12 +0300

    Linting

--- src/routes/User/Account/deleteAccount.js
+++ src/routes/User/Account/deleteAccount.js
@@ -3,8 +3,8 @@ const { handleError500 } = require("../../../library/errorHandler");
 
 module.exports = (req, res) => {
   if (!req.params.id) res.status(400).send("Not All Parameters Given.");
-  if (!req.user) res.status(403).send("Not Logged In.");
-  if (req.user.id !== req.params.id) res.status(401).send("Not Authorized.");
+  else if (!req.user) res.status(403).send("Not Logged In.");
+  else if (req.user.id !== req.params.id) res.status(401).send("Not Authorized.");
   else {
     User.findOne(
       {
@@ -15,26 +15,24 @@ module.exports = (req, res) => {
       },
     )
       .then((userData) => {
-        if (!userData) return res.status(404).send("User Not Found.");
-
-        compare(
-          req.body.password,
-          userData.password,
-          (err, result) => {
-            if (err) handleError500(err);
-            else if (!result) return res.status(403).send("Incorrect Password.");
-            else {
-              userData
-                .destroy()
-                .then(() => res.status(200).send("Success."))
-                .catch((error) => handleError500(error));
-            }
-          },
-        );
+        if (!userData) res.status(404).send("User Not Found.");
+        else {
+          compare(
+            req.body.password,
+            userData.password,
+            (err, result) => {
+              if (err) handleError500(err);
+              else if (!result) res.status(403).send("Incorrect Password.");
+              else {
+                userData
+                  .destroy()
+                  .then(() => res.status(200).send("Success."))
+                  .catch((error) => handleError500(error));
+              }
+            },
+          );
+        }
       })
-      .catch((error) => {
-        console.log(error);
-        return res.status(500).send("Internal Server Error.");
-      });
+      .catch((error) => handleError500(error));
   }
 };
