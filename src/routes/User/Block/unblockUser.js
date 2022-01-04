c14a71856150dda89b0c19636f5635571b71b9dc
Neil <30356902+NeilChaudhari@users.noreply.github.com>
2022-01-04 08:55:03 -0500

    💦Unblock User Endpoint

---
+++ src/routes/User/Block/unblockUser.js
@@ -0,0 +1,38 @@
+const { handleError500 } = require("../../../library/errorHandler");
+
+module.exports = (req, res) => {
+  if (!req.user) res.status(403).send("Not Logged In.");
+  if (!req.params.id || !req.params.blockId) res.status(400).send("Not All Parameters Provided.");
+  else {
+    User.findOne(
+      {
+        where:
+        {
+          id: req.params.blockId,
+        },
+      },
+    )
+      .then((blockData) => {
+        User.findOne(
+          {
+            where:
+            {
+              id: req.user.id,
+            },
+          },
+        )
+          .then((userData) => {
+            if (!userData.hasBlocked(blockData)) {
+              res.status(401).send("You have not blocked this person");
+            } else {
+              blockData
+                .removeBlocked(blockData)
+                .then(() => res.json(userData))
+                .catch((error) => handleError500(error));
+            }
+          })
+          .catch((error) => handleError500(error));
+      })
+      .catch((error) => handleError500(error));
+  }
+};
