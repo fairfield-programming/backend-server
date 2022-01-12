c278aabd9fab4852aa44a9635420a8605fb684ad
William McGonagle <wpmcgonagle@gmail.com>
2022-01-12 08:52:22 -0500

    ✏️ Changed Root to Redirect

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,6 @@
-const { readFile } = require("fs/promises")
-const address = '../../../fairfield-programming.github.io/src/index.html'
 module.exports = (req, res) => {
-    readFile(address, 'utf-8', (error, html)=>{
-        res.send(html)
-    }).catch((error)=>{
-        console.log(error);
-        return res.status(500).send("Internal Server Error.");
-    })
+
+    // Redirect to Homepage
+    res.setHeader('Location', "https://fairfieldprogramming.org/");    
+
 }
