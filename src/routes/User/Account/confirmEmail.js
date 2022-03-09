d8438d817dd884e7d87f3dbe393172bb5ee54d7b
MassiGy <ghernaoutmassi@gmail.com>
2022-03-09 22:52:12 +0100

    User Redirection Setup
    If the user is attempting to validate his email address, so he is now on
    his mailbox, and he clicked on the link that we've sent to him, so he is
    currently performing an http get request to our backend, and this
    controller will handle that.
    
    After handling it, we need to redirect the user, so basicly what I did
    is that I check if the user is already loggedin not a long time ago (his
    jwt is available in the cookies). If so, we redirect him to the home page. If not,
    we redirect him to the login page.
    
    Hope that is clear !
    Please refer to me if something is wrong.
    
    Massiles.

--- src/routes/User/Account/confirmEmail.js
+++ src/routes/User/Account/confirmEmail.js
@@ -3,13 +3,15 @@ const { verify } = require("jsonwebtoken");
 module.exports = async (req, res) => {
     try {
         // get the user id from the jwt on the req.params
-        let id = verify(req.params.token, process.env.Email_Token_Signature)
+        const id = verify(req.params.token, process.env.Email_Token_Signature)
 
         // fetch our db for the corresponding user record, and set the email_confirmed attribute to true;
         await User.update({ confirmed_email: true }, { where: { id: id } }); // use the global User object
 
-        res.send("Your Email had be validated, please go and login so be active on the platform !");
-
+        // check if the user token is availibe inside a cookie 
+        // this means that the user has logged in to our platform, and still has a not expired cookie
+        if (req.cookies.token) res.redirect("/");
+        else res.redirect("/login");
     } catch (err) {
         res.status(500).send(err.message);
     }
