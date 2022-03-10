dec0dfd78b076c6258ba72244dc4646d2fb939a8
MassiGy <ghernaoutmassi@gmail.com>
2022-03-10 22:55:15 +0100

    Accessing User ID directly from the token

--- src/routes/User/Account/confirmEmail.js
+++ src/routes/User/Account/confirmEmail.js
@@ -3,7 +3,9 @@ const { verify } = require("jsonwebtoken");
 module.exports = async (req, res) => {
     try {
         // get the user id from the jwt on the req.params
-        const id = verify(req.params.token, process.env.Email_Token_Signature)
+        console.log(req.params.token);
+        const id = verify(req.params.token, process.env.Email_Token_Signature).id;
+        console.log(id);
 
         // fetch our db for the corresponding user record, and set the email_confirmed attribute to true;
         await User.update({ confirmed_email: true }, { where: { id: id } }); // use the global User object
