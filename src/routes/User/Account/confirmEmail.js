f8f81fa6009942dc081d531077f78b46e5e1e291
William McGonagle <wpmcgonagle@gmail.com>
2022-03-12 10:21:23 -0500

    Fixed Redirect

--- src/routes/User/Account/confirmEmail.js
+++ src/routes/User/Account/confirmEmail.js
@@ -2,22 +2,20 @@ const { verify } = require("jsonwebtoken");
 
 module.exports = async (req, res) => {
     try {
-        
+
         // get the user id from the jwt on the req.params
         const id = verify(req.params.token, process.env.EMAIL_TOKEN).id;
 
         // fetch our db for the corresponding user record, and set the email_confirmed attribute to true;
         await User.update({ confirmed_email: true }, { where: { id: id } }); // use the global User object
 
-        // check if the user token is availibe inside a cookie 
-        // this means that the user has logged in to our platform, and still has a not expired cookie
-        if (req.cookies.token) res.redirect("/");
-        else res.redirect("/login");
+        // if we redirect to login, it will redirect home if already logged in
+        res.redirect("https://fairfieldprogramming.org/auth/login");
 
     } catch (err) {
 
         console.log(err);
-        res.status(500).send("Internal Server Error.");
+        res.redirect("https://fairfieldprogramming.org/auth/login");
     
     }
 }
