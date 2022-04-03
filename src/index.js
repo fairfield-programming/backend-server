ba4ac28c06a677af86e9be2173a85c0c9a6a7184
William McGonagle <wpmcgonagle@gmail.com>
2022-04-03 19:37:03 -0400

    Changed CORS to More Liberal

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 8080;
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(require("cors")({ origin: "https://fairfieldprogramming.org" }));
+app.use(require("cors")());
 
 
 
