d6c92af7bdbd5d1352bc51c70190456819d27b51
MassiGy <ghernaoutmassi@gmail.com>
2022-05-04 19:51:47 +0200

    remove unneeded await to speed up code execution

--- src/controllers/User/Account/confirmEmail.js
+++ src/controllers/User/Account/confirmEmail.js
@@ -14,13 +14,13 @@ const { verify } = require("jsonwebtoken");
  */
 
 
-module.exports.confirmEmail = async (req, res) => {
+module.exports.confirmEmail =  (req, res) => {
 	try {
 		// get the user id from the jwt on the req.params
 		const id = verify(req.params.token, process.env.EMAIL_TOKEN).id;
 
 		// fetch our db for the corresponding user record, and set the email_confirmed attribute to true;
-		await User.update({ confirmed_email: true }, { where: { id: id } }); // use the global User object
+		User.update({ confirmed_email: true }, { where: { id: id } });
 
 		// if we redirect to login, it will redirect home if already logged in
 		res.redirect('https://fairfieldprogramming.org/auth/login');
